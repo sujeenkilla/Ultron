59bf647b2d11d5eea213d4922c7894441006d0d3
vikram <sujeenlucky9199@gmail.com>
2025-07-03 08:12:27 +0530

    Adding New Feature of Handeling CharSequnece

---
+++ src/kernel/stdio.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "print.h"
+
+void putc(char c){
+    x86_Viedo_WriteCharTeletype(c, 0);
+}
+
+void puts(const char* s){
+    while(*s){
+        putc(*s);
+        s++;
+
+    }
+}
